dd084c2a44ab58f4c114c59ee662ad028585bf70
McJosh <truongduydat2402@gmail.com>
2026-04-27 16:47:01 +0700

    hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/Hero.css"
+
+const Hero = () => {
+  return (
+    <section>
+        <div className="container">
+            <h1>Công cụ tính Thuế thu nhập cá nhân chuẩn 2026</h1>
+            <h2>Cập nhật mới nhất: 27/04/2026</h2>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
