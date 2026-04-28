531cfed2a7e76e7af5687078aeb5860b62cd9cfa
McJosh <truongduydat2402@gmail.com>
2026-04-28 17:17:41 +0700

    install tailwindcss

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import "../styles/Hero.css"
 
 const Hero = () => {
   return (
     <section>
-        <div className="container">
+        <div className="hero">
             <h1>Công cụ tính Thuế thu nhập cá nhân chuẩn 2026</h1>
             <h2>Cập nhật mới nhất: 27/04/2026</h2>
         </div>
